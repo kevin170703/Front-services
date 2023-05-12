4257e4a6c3b4b81e399c725a297d14dab3175278
kevin170703 <correakevinfabian01@gmail.com>
2023-05-12 17:16:18 -0300

    modificando funcionalidad de boton publicar

--- src/componentes/Landing/Landing.jsx
+++ src/componentes/Landing/Landing.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import style from "./Landing.module.css";
+import { useSelector } from "react-redux";
 
 export default function Landing() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className={style.contetAll}>
       <NavBar landing={true} />
@@ -18,15 +21,14 @@ export default function Landing() {
           <Link to="/home" className={style.buttonServices}>
             Servicios
           </Link>
-          <Link to="/createPublication" className={style.buttonPosts}>
+
+          <Link
+            to={!user.length ? "/login" : "/createPublication"}
+            className={style.buttonPosts}
+          >
             Publicar
           </Link>
         </div>
-
-        {/* <div className={style.contentImg}>
-          <img src={fondo} alt="" />
-        </div>
-        <div className={style.wave}></div> */}
       </div>
     </div>
   );
