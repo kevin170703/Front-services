b301fc42a03da781c02d1da0ae4da28bbe2598a7
kevin170703 <correakevinfabian01@gmail.com>
2023-05-12 15:37:53 -0300

    migrando a vite services 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import Home from "./componentes/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./componentes/Landing/Landing";
+import Login from "./componentes/Login/Login";
+import Register from "./componentes/Register/Register";
+import CreatePublic from "./componentes/CreatePublic/CreatePublic";
+import Profile from "./componentes/Profile/Profile";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API || "http://localhost:3001";
 
 function App() {
   return (
-    <>
-      <h1>hola</h1>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/createPublication" element={<CreatePublic />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
