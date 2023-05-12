f0775e17a5f427c010fae659b5dd24cd782097d3
kevin170703 <correakevinfabian01@gmail.com>
2023-05-12 15:36:40 -0300

    migrando a vite services 2.0

---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <div>
+        <h6>contactos de el creador:</h6>
+      </div>
+    </div>
+  );
+}
