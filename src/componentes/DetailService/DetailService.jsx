e88aaea87652457e97962b120bca5032bc32d675
kevin170703 <correakevinfabian01@gmail.com>
2023-05-15 20:28:55 -0300

    añadiendo componente de detalles

---
+++ src/componentes/DetailService/DetailService.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import style from "./DetailService.module.css";
+import { CiLocationOn, CiUser, CiBadgeDollar } from "react-icons/ci";
+
+export default function DetailService({
+  title,
+  servicesFor,
+  location,
+  contact,
+  price,
+  id,
+}) {
+  return (
+    <div className={style.contentAll}>
+      <h5>{title}</h5>
+
+      <div className={style.location}>
+        <CiLocationOn size="30" />
+        <p>{location}</p>
+      </div>
+
+      <div className={style.contentInfoUser}>
+        <div className={style.contentPriceUserHour}>
+          <CiUser size="30" />
+          <p>{servicesFor}</p>
+        </div>
+
+        <div className={style.contentPriceUserHour}>
+          <CiBadgeDollar size="30" />
+          <p>{price}</p>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur
+        quos obcaecati ea quasi voluptatibus magni iure maxime error nisi,
+        quidem dolor quam. Aperiam eveniet optio nostrum corporis? Culpa,
+        quidem.
+      </p>
+      <a
+        href={`https://wa.me/${contact}`}
+        target="_blank"
+        className={style.buttonContanct}
+      >
+        Contactar
+      </a>
+    </div>
+  );
+}
