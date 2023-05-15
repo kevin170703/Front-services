9818053f12a9ac79eb949bb3c995b0c3cc0baa85
kevin170703 <correakevinfabian01@gmail.com>
2023-05-15 20:29:04 -0300

    actualizando a la nueva version

--- src/componentes/Login/Login.jsx
+++ src/componentes/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import style from "./Login.module.css";
 import { NavLink, useNavigate } from "react-router-dom";
-import fondo from "../../assets/undraw_profile_re_4a55.svg";
 import { login } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
@@ -12,20 +11,10 @@ export default function Login() {
   const distpach = useDispatch();
   const navegation = useNavigate();
   const [loginData, setLoginData] = useState({ password: "", phoneNumber: "" });
-  const [error, setError] = useState({});
 
   function handelLoginData(e) {
     e.preventDefault();
     setLoginData({ ...loginData, [e.target.name]: e.target.value });
-    setError(validate({ ...loginData, [e.target.name]: e.target.value }));
-  }
-
-  function validate(loginData) {
-    let error = {};
-    if (!loginData.phoneNumber)
-      error.phoneNumber = "Debe ingresar el numero de telefono";
-    if (!loginData.password) error.password = "Debe ingresar la contraseña";
-    return error;
   }
 
   function handelLoginSend(e) {
@@ -39,39 +28,32 @@ export default function Login() {
       <NavLink to="/">
         <BiLeftArrowAlt size="40" className={style.back} />
       </NavLink>
+      <div className={style.backgroundCircle}>
+        <h2>Services</h2>
+        <p>Conectando Personas y Servicios</p>
+      </div>
       <div className={style.content}>
         <form className={style.inputs} onSubmit={(e) => handelLoginSend(e)}>
+          <h1>Iniciar sesion</h1>
           <div className={style.contentInputs}>
-            <label>Numero de telefono</label>
             <input
               type="text"
               name="phoneNumber"
+              placeholder="Numero de telefono"
               value={loginData.phoneNumber}
               onChange={(e) => handelLoginData(e)}
             />
-            {error.phoneNumber && (
-              <p className={style.errors}>{error.phoneNumber}</p>
-            )}
           </div>
           <div className={style.contentInputs}>
-            <label>Contraseña</label>
             <input
               type="password"
               name="password"
+              placeholder="Constraseña"
               value={loginData.password}
               onChange={(e) => handelLoginData(e)}
             />
-            {error.password && <p className={style.errors}>{error.password}</p>}
-            {error.data && <p className={style.errors}>{error.data}</p>}
           </div>
-          <button
-            type="submit"
-            className={
-              !error.phoneNumber && !error.password
-                ? style.iniciar
-                : style.iniciarNone
-            }
-          >
+          <button type="submit" className={style.iniciar}>
             iniciar sesion
           </button>
           <p>
@@ -83,9 +65,6 @@ export default function Login() {
             }
           </p>
         </form>
-        <div className={style.fondo}>
-          <img src={fondo} alt="" />
-        </div>
       </div>
     </div>
   );
