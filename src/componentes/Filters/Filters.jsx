6bdc02d06ea454cad74d9c8f27a6aec46183fa80
kevin170703 <correakevinfabian01@gmail.com>
2023-05-30 15:22:06 -0300

    creando componente par los filters

---
+++ src/componentes/Filters/Filters.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "./Filters.module.css";
+import useFilters from "../../hooks/useFilters";
+import countries from "../../assets/countries.json";
+
+export default function Filters() {
+  const { applyFilters, filters, setFilters } = useFilters();
+  return (
+    <form className={style.filters} onSubmit={(e) => applyFilters(e)}>
+      <input
+        type="search"
+        list="listCounties"
+        placeholder="Pais"
+        value={filters.country}
+        onChange={(e) => setFilters({ country: e.target.value })}
+      />
+
+      <datalist id="listCounties">
+        {countries.map((country) => (
+          <option value={country.name} key={country.id}>
+            {country.name}
+          </option>
+        ))}
+      </datalist>
+      <button type="submit">Aplicar</button>
+    </form>
+  );
+}
