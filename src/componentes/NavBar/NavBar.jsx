cddb3c83a9a3b721e57008eecc5faa34aa211344
kevin170703 <correakevinfabian01@gmail.com>
2023-05-30 15:21:04 -0300

    mejorarndo responsive

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ import { searchName } from "../../redux/actions";
 
 import { CiSearch } from "react-icons/ci";
 import { HiBars3, HiXMark } from "react-icons/hi2";
-import { CiUser } from "react-icons/ci";
+import { CiUser, CiCirclePlus, CiHome, CiLogin } from "react-icons/ci";
 
 export default function Navbar({ landing, services }) {
   const user = useSelector((state) => state.user);
@@ -44,6 +44,7 @@ export default function Navbar({ landing, services }) {
           className={style.butonMenu}
           style={{
             display: showMenuPhone ? "flex" : "none",
+            color: "#fff",
           }}
           onClick={() => setShowMenuPhone(false)}
         />
@@ -52,51 +53,62 @@ export default function Navbar({ landing, services }) {
       {/* ------------------------------------------- */}
 
       <div className={showMenuPhone ? style.contentVistPhone : style.content}>
-        <div className={style.navegation}>
-          <NavLink to="/" className={style.inicio}>
-            Services
-          </NavLink>
+        <div className={style.contentMenu}>
+          <div className={style.navegation}>
+            <NavLink to="/" className={style.inicio}>
+              <CiHome className={style.logoHome} />
+              Services
+            </NavLink>
+
+            {!user.length ? (
+              <NavLink
+                className={style.publishServices}
+                onClick={() => setShowModal(true)}
+              >
+                <CiCirclePlus className={style.logoCreateService} />
+                Publicar servicios
+              </NavLink>
+            ) : (
+              <NavLink
+                className={style.publishServices}
+                to="/createPublication"
+              >
+                <CiCirclePlus className={style.logoCreateService} />
+                Publicar servicios
+              </NavLink>
+            )}
+          </div>
+
+          {services && (
+            <form className={style.form} onSubmit={(e) => dataSearch(e)}>
+              <input
+                type="text"
+                placeholder="Carpintero, Plomero..."
+                value={nameSearch}
+                onChange={(e) => setNameSearch(e.target.value)}
+              />
+              <button type="submit">{<CiSearch size="30" />}</button>
+            </form>
+          )}
 
           {!user.length ? (
-            <NavLink
-              className={style.publishServices}
-              onClick={() => setShowModal(true)}
-            >
-              Publicar servicios
-            </NavLink>
+            <div className={style.sesionAndPosts}>
+              <NavLink to="/login" className={style.sesion}>
+                <CiLogin className={style.logoLogin} />
+                Iniciar sesion
+              </NavLink>
+              <Link to="/register" className={style.registerButton}>
+                <CiUser className={style.logoRegister} />
+                Registrarse
+              </Link>
+            </div>
           ) : (
-            <NavLink className={style.publishServices} to="/createPublication">
-              Publicar servicios
+            <NavLink to="/profile" className={style.contentLinkProfile}>
+              <CiUser size="40" className={style.logoProfile} />
+              <p>Perfil</p>
             </NavLink>
           )}
         </div>
-
-        {services && (
-          <form className={style.form} onSubmit={(e) => dataSearch(e)}>
-            <input
-              type="text"
-              placeholder="Carpintero, Plomero..."
-              value={nameSearch}
-              onChange={(e) => setNameSearch(e.target.value)}
-            />
-            <button type="submit">{<CiSearch size="30" />}</button>
-          </form>
-        )}
-
-        {!user.length ? (
-          <div className={style.sesionAndPosts}>
-            <NavLink to="/login" className={style.sesion}>
-              Iniciar sesion
-            </NavLink>
-            <Link to="/register" className={style.registerButton}>
-              Registrarse
-            </Link>
-          </div>
-        ) : (
-          <NavLink to="/profile">
-            <CiUser size="40" className={style.logoProfile} />
-          </NavLink>
-        )}
       </div>
     </div>
   );
