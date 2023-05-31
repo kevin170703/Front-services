9b09ae5225dbbca9e2e022049d4ab20960df0e61
kevin170703 <correakevinfabian01@gmail.com>
2023-05-31 09:50:36 -0300

    mejorando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import Loader from "./componentes/Loader/Loader";
 const Landing = lazy(() => import("./componentes/Landing/Landing"));
-const Home = lazy(() => import("./componentes/Home/Home"));
+const Services = lazy(() => import("./componentes/Services/Services"));
 const Login = lazy(() => import("./componentes/Login/Login"));
 const Register = lazy(() => import("./componentes/Register/Register"));
-const CreatePublic = lazy(() =>
-  import("./componentes/CreatePublic/CreatePublic")
+const CreateService = lazy(() =>
+  import("./componentes/CreateService/CreateService")
 );
 const Profile = lazy(() => import("./componentes/Profile/Profile"));
 
@@ -15,14 +16,14 @@ axios.defaults.baseURL = import.meta.env.VITE_API || "http://localhost:3001";
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Cargando</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Services />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/createPublication" element={<CreatePublic />} />
+          <Route path="/createService" element={<CreateService />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/home" element={<Home />} />
         </Routes>
       </Suspense>
     </div>
