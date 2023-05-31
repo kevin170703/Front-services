9e3b6b8e5e29a2e6e3829f30ec776f8df7bc60db
kevin170703 <correakevinfabian01@gmail.com>
2023-05-30 22:17:14 -0300

    optimizando la plataforma

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import Home from "./componentes/Home/Home";
-import { Routes, Route } from "react-router-dom";
-import Landing from "./componentes/Landing/Landing";
-import Login from "./componentes/Login/Login";
-import Register from "./componentes/Register/Register";
-import CreatePublic from "./componentes/CreatePublic/CreatePublic";
-import Profile from "./componentes/Profile/Profile";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+const Landing = lazy(() => import("./componentes/Landing/Landing"));
+const Home = lazy(() => import("./componentes/Home/Home"));
+const Login = lazy(() => import("./componentes/Login/Login"));
+const Register = lazy(() => import("./componentes/Register/Register"));
+const CreatePublic = lazy(() =>
+  import("./componentes/CreatePublic/CreatePublic")
+);
+const Profile = lazy(() => import("./componentes/Profile/Profile"));
+
 axios.defaults.baseURL = import.meta.env.VITE_API || "http://localhost:3001";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/createPublication" element={<CreatePublic />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<div>Cargando</div>}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/createPublication" element={<CreatePublic />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
