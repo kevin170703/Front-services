3d5aec0c74d71e090a93ea38de581c35bb9a2f54
kevin170703 <correakevinfabian01@gmail.com>
2023-06-06 10:15:35 -0300

    mejorando responsive

--- src/componentes/Filters/Filters.jsx
+++ src/componentes/Filters/Filters.jsx
@@ -6,31 +6,33 @@ import countries from "../../assets/countries.json";
 export default function Filters() {
   const { applyFilters, filters, setFilters } = useFilters();
   return (
-    <form className={style.filters} onSubmit={(e) => applyFilters(e)}>
-      <input
-        type="search"
-        list="listCounties"
-        placeholder="Pais"
-        value={filters.country}
-        onChange={(e) => setFilters({ country: e.target.value })}
-      />
+    <section className={style.filters}>
+      <form className={style.form} onSubmit={(e) => applyFilters(e)}>
+        <input
+          type="search"
+          list="listCounties"
+          placeholder="Pais"
+          value={filters.country}
+          onChange={(e) => setFilters({ country: e.target.value })}
+        />
 
-      <datalist id="listCounties">
-        {countries.map((country) => (
-          <option value={country.name} key={country.id}>
-            {country.name}
-          </option>
-        ))}
-      </datalist>
-      <button type="submit" className={style.buttonSubmit}>
-        Aplicar
-      </button>
-      <button
-        className={style.buttonUndo}
-        onClick={() => setFilters({ country: "" })}
-      >
-        deshacer
-      </button>
-    </form>
+        <datalist id="listCounties">
+          {countries.map((country) => (
+            <option value={country.name} key={country.id}>
+              {country.name}
+            </option>
+          ))}
+        </datalist>
+        <button type="submit" className={style.buttonSubmit}>
+          Aplicar
+        </button>
+        <button
+          className={style.buttonUndo}
+          onClick={() => setFilters({ country: "" })}
+        >
+          deshacer
+        </button>
+      </form>
+    </section>
   );
 }
